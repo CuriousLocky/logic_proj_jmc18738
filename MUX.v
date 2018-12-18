8aeb5d3f1cc1f788fe835fcb5f4ee33c7fed577b
martinkayondo <kayondomartin45@gmail.com>
2018-12-18 15:00:44 +0900

    designed a 16_bit mux

---
+++ MUX.v
@@ -0,0 +1,21 @@
+/*
+Simple design for a 2:1 multiplexer
+Set selector to 0 for the first inputA,
+else you will get the second input,
+*/
+
+module MUX16Bits(
+    input [15:0] inA,
+    input [15:0] inB,
+
+    input selector,
+
+    output [15:0] result
+);
+    initial begin
+        if(selector == 0)
+            result <= inA;
+        else
+            result <= inB;
+    end
+endmodule
